f957cb485eaa742c4db8020a3c4d435ba791fbd5
Hrangett <100149118+Hrangett@users.noreply.github.com>
2022-03-18 11:05:01 +0900

    이동생성자

--- ch06/00_이동생성자.cpp
+++ ch06/00_이동생성자.cpp
@@ -20,9 +20,23 @@ public:
 	{
 		strcpy(this->name, name);
 		this->age = age;
-		cout << "생성자 호출\n" << endl;
+		cout << "생성자 호출" << endl;
 	}
-	
+
+	Human(const Human& other)
+	{//복사생성자 :: Lvalue 참조이므로 &
+		cout << "\n복사생성자 호출" << endl;
+		strcpy(this->name, other.name);
+		this->age = other.age;
+
+	}
+
+	Human(Human&& other) noexcept
+	{//이동생성자 호출 :: Rvalue 참조이므로 &&
+		cout << "\n이동생성자 호출" << endl;
+		strcpy(this->name, other.name);
+		this->age = other.age;
+	}
 
 };
 
@@ -40,6 +54,12 @@ int main()
 
 	Human h1("홍길동", 30);
 	h1.viewHuman();
+
+	Human h2(h1);
+	h2.viewHuman();
+
+	Human h3(move(h1));//lvalue type인 h1을 rvalue형태로 바꿔서 return
+	h3.viewHuman();
 
 	return 0;
 }
--- ch06/08_ConstStaticMember.cpp
+++ ch06/08_ConstStaticMember.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 using namespace std;
 
+//클래스 내에서의 변수초기화는 원칙적으론 불가능이지만,
+//const static은 클래스가 정의될 때 지정된 값이 유지되는 상수이기 떄문에
+//const static으로 맴버변수를 선언함으로써 초기화가 가능하도록 문법으로 정의
+//가급적 사용하지 않는다.
+
 class CountryArea
 {
 public:
